24ca86de57996bc7019b8846042f79893edaa627
techdogie <68420489+techdogie@users.noreply.github.com>
2022-09-03 14:41:14 +0530

    Update config.php
    6

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'database-1.ci5r9dhgc2ko.ap-south-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', '123456789');
+define('DB_PASSWORD', '12345789');
 define('DB_NAME', 'employees_db');
  
 /* Attempt to connect to MySQL database */
